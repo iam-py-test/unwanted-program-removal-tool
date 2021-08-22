6cf0ce25cb7fc19b7e2a252abaefb055126a5e21
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-08-22 16:16:15 -0400

    Update search.js

--- search.js
+++ search.js
@@ -5,8 +5,14 @@ if(new URL(location).searchParams.get("dname") !== null){
 console.log(param)
 var main = async function(){
   var datareq = await fetch("https://raw.githubusercontent.com/iam-py-test/unwanted-program-removal-tool/main/data_1.json")
-  var data = JSON.stringify(await datareq.text())
+  var data = JSON.parse(await datareq.text())
   console.log(data)
-  
+  try{
+    console.log(data[param])
+    document.getElementById("desc").innerText = data[param].desc
+  }
+  catch(err){
+    console.log(err)
+  }
 }
 main()
