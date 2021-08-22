653c3c270c3c0c3f6aae94f2c032b06d46b06cca
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-08-22 16:12:13 -0400

    Create search.js

---
+++ search.js
@@ -0,0 +1,12 @@
+var param = null
+if(new URL(location).searchParams.get("dname") !== null){
+  param = new URL(location).searchParams.get("dname")
+}
+console.log(param)
+var main = async function(){
+  var datareq = await fetch("https://raw.githubusercontent.com/iam-py-test/unwanted-program-removal-tool/main/data_1.json")
+  var data = JSON.stringify(await datareq.text())
+  console.log(data)
+  
+}
+main()
