54c03096f07fef26c4d919019980d3e78731b9b5
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-08-28 18:15:58 -0400

    Make the scans faster

--- scanner.py
+++ scanner.py
@@ -3,6 +3,8 @@ import requests
 import json
 from hashlib import sha256
 
+version = 1.0
+
 dirtoscan = input("Enter to dir to scan: ")
 
 sigs = json.loads(requests.get('https://raw.githubusercontent.com/iam-py-test/unwanted-program-removal-tool/main/sha256_sigs.json').text)
@@ -10,35 +12,35 @@ detectedfiles = []
 
 newsigs = {}
 for cata in sigs:
-  for detection in sigs[data]:
-    newsigs[detection] = sigs[cata]
+	for detection in sigs[cata]:
+		newsigs[detection] = sigs[cata][detection]
   
-print(newsigs)
+with open("sigs_1.txt","w") as f:
+	f.write("{}".format(newsigs))
+	f.close()
 
 for root,dirs,files in os.walk(dirtoscan):
-  for file in files:
-      try:
-        sha256f = sha256(open(os.path.join(root,file),"rb").read()).hexdigest()
-        for cata in sigs:
-          for detection in sigs[cata]:
-            cataA = sigs[cata]
-            if sha256f in cataA[detection]:
-              print("File {} in {} has been detected as {}".format(file,root,detection))
-              detectedfiles.append({"path":os.path.join(root,file),"detection":detection})
-              try:
-                if input("Remove (y/n): ") == 'y':
-                    try:
-                      import subprocess
-                      subprocess.run("taskkill /F /IM \"{}\"".format(file),shell=True)
-                    except:
-                      pass
-                    else:
-                      print("Process {} ended. Removing file...".format(file))
-                    os.remove(os.path.join(root,file))
-              except:
-                print("Failed to remove file")
-      except Exception as err:
-        pass
+	for file in files:
+		try:
+			sha256f = sha256(open(os.path.join(root,file),"rb").read()).hexdigest()
+			for sig in newsigs:
+				if sha256f in newsigs[sig]:
+					print("File {} in {} has been detected as {}".format(file,root,sig))
+					detectedfiles.append({"path":os.path.join(root,file),"detection":sig})
+					try:
+						if input("Remove (y/n): ") == 'y':
+							try:
+								import subprocess
+								subprocess.run("taskkill /F /IM \"{}\"".format(file),shell=True)
+							except:
+								pass
+							else:
+								print("Process {} ended. Removing file...".format(file))
+							os.remove(os.path.join(root,file))
+					except:
+						print("Failed to remove file")
+		except Exception as err:
+			pass
  
 print("\n\n\nDetected malware:\n")
 for detection in detectedfiles:
